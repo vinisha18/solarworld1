179411925f16000a84741988bd35241c58f7d5f7
vinisha18 <vinisha3114@gmail.com>
2016-07-02 22:28:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Vinisha on 7/2/2016.
+ */
+document.getElementById("foot01").innerHTML=
+    "<p>&copy; "+new Date().getFullYear()+" Solarduniya. All rights reserved.</p>";
+document.getElementById("nav01").innerHTML=
+    "<ul id='menu'>"+
+        "<li><a href='index.html'>Home</a> </li>"+
+        "<li><a href='products.html'>Our Products</a> </li>"+
+        "<li><a href='about.html'>About Us</a></li>"+
+        "</ul>"
